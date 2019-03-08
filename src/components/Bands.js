7d314d4a841e5aa2699da6c7e3169cf0d5ccf77e
Christopher Tompkins <chtompkins026@gmail.com>
2019-03-08 17:43:52 +0000

    Automatically backed up by Learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 const Bands = props => {
   const bands = props.bands.map((band, index) => {
-    return <li key={index}>{band}</li>
+    return <li key={index}>{band.name}</li>;
   });
 
-  return(
+  return (
     <div>
       {bands}
     </div>
-  )
-}
+  );
 
-export default Bands
+};
+
+export default Bands;
